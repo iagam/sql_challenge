bf5fe96b87c060e28bbdf1adbd852a8d12bc8a79
iagam <iamagam03@gmail.com>
2022-03-06 02:33:16 +0530

    Started challenge 3

---
+++ Challenge_3.sql
@@ -0,0 +1,8 @@
+/* For the year 2012, create a 3 column, 1 row report showing the percent share of gdp_per_capita for the following regions:
+
+(i) Asia, (ii) Europe, (iii) the Rest of the World. Your result should look something like
+
+ Asia  | Europe | Rest of World 
+------ | ------ | -------------
+25.0%  | 25.0%  | 50.0%
+*/
